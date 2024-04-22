ca04256cd8ade004ba77977036842331d1be6c0b
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-04-22 17:06:49 +0530

    added build command for live server

--- test.js
+++
@@ -1,7 +0,0 @@
-// const {passwordEncrypt,passwordMatching}=require("./backend/config/passwordEncyptDecrypt");
-
-// const password="hello";
-// const hashPassword=passwordEncrypt(password);
-// const match=passwordMatching(password,hashPassword);
-// console.log(match+" "+hashPassword);
-
