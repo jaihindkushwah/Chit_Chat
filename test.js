5c92aa1ae1d488e201ca740075032cb7da2a843d
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-04-22 15:40:24 +0530

    added fr

---
+++ test.js
@@ -0,0 +1,7 @@
+// const {passwordEncrypt,passwordMatching}=require("./backend/config/passwordEncyptDecrypt");
+
+// const password="hello";
+// const hashPassword=passwordEncrypt(password);
+// const match=passwordMatching(password,hashPassword);
+// console.log(match+" "+hashPassword);
+
